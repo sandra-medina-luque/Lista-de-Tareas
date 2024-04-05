9de800d1c0a49f09e9bbc9bf2d6a6ec53529ad87
sandra-medina-luque <153823146+sandra-medina-luque@users.noreply.github.com>
2024-04-05 18:43:14 +0200

    css

--- ListaTareas/src/components/Task.jsx
+++ ListaTareas/src/components/Task.jsx
@@ -2,43 +2,49 @@ import React, { useState } from 'react'
 import './task.css'
 
 const Task = ({ task, deletedTask, updateTask }) => {
-  const [completed, setCompleted] = useState(false)
-  const [editable, setEditable] = useState(false)
-  const [editedTask, setEditedTask] = useState(task.task)
-
-  const handleEdit = () => {
-    setEditable(true)
-  }
-
-  const handleSave = () => {
-    if (editedTask.trim() === '') {
-      alert('Debes escribir algo')
-      return
-    }
-    updateTask(task.id, editedTask)
-    setEditable(false)
-  }
-
-  return (
-    <div className={completed ? 'containerTask containerTaskCompleted' : 'containerTask'}>
-      {editable ? (
-        <input
-          type="text"
-          value={editedTask}
-          onChange={(e) => setEditedTask(e.target.value)}
-          onBlur={handleSave}
-          autoFocus
-        />
-      ) : (
-        <h2 className={completed ? 'completed' : 'npCompleted'}>{task.task}</h2>
-      )}
-      <button id='complete' onClick={() => setCompleted(!completed)}>
-        {completed ? 'No completada' : 'Completada'}
-      </button>
-      <button onClick={handleEdit} id='edit'>Editar</button>
-      <button onClick={() => deletedTask(task.id)} id='delete'>Eliminar</button>
-    </div>
-  )
-}
-
-export default Task
+    const [completed, setCompleted] = useState(false);
+    const [editable, setEditable] = useState(false);
+    const [editedTask, setEditedTask] = useState(task.task);
+  
+    const handleEdit = () => {
+      setEditable(true);
+    };
+  
+    const handleSave = () => {
+      if (editedTask.trim() === '') {
+        alert('Debes escribir algo');
+        return;
+      }
+      updateTask(task.id, editedTask);
+      setEditable(false);
+    };
+  
+   
+  
+    return (
+      <div className={`task-card ${completed ? 'completed' : ''}`}>
+        <div className="task-content">
+          {editable ? (
+            <input
+              type="text"
+              value={editedTask}
+              onChange={(e) => setEditedTask(e.target.value)}
+              onBlur={handleSave}
+              autoFocus
+            />
+          ) : (
+            <h2>{task.task}</h2>
+          )}
+          <div className="task-buttons">
+            <button onClick={() => setCompleted(!completed)}>
+              {completed ? 'Completada' : 'No Completada'}
+            </button>
+            <button onClick={handleEdit}>Editar</button>
+            <button onClick={() => deletedTask(task.id)}>Eliminar</button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Task;
